e7c96bea73dcf02eb19b158cb4c2ff3f6fcda0b9
Franklin van de Meent <fr@nkl.in>
2015-09-07 20:52:52 +0200

    Clean code style
    * Indentation by 2 spaces
    
    * Space after keywords, control struct
    
    * Semicolons
    
    * Removed trailing whitespace
    
    * Strict conditionals
    
    * Minor changes

--- requestbin.js
+++ requestbin.js
@@ -1,82 +1,101 @@
-var http = require('http'),
-    querystring = require('querystring')
+var http = require ('http');
+var querystring = require ('querystring');
 
 module.exports = {
-    create:     function( isPrivate, callback ) {
-        var props = {}
-        if( typeof isPrivate === 'function' ) {
-            var callback = isPrivate
-        } else if( isPrivate === true ) {
-            props.private = 'true'
-        }
-        talk( 'POST', 'bins', props, callback )
-    },
-    get:        function( bin, callback ) { talk( 'GET', 'bins/'+ bin, callback ) },
-    requests:   function( bin, callback ) { talk( 'GET', 'bins/'+ bin +'/requests', callback ) },
-    request:    function( bin, request, callback ) { talk( 'GET', 'bins/'+ bin +'/requests/'+ request, callback ) }
-}
+  create: function (isPrivate, callback) {
+    var props = {};
 
-function talk( method, path, props, callback ) {
-    if( typeof props === 'function' ) {
-        var callback = props
-        var props = {}
+    if (typeof isPrivate === 'function') {
+      var callback = isPrivate;
+    } else if (isPrivate === true) {
+      props.private = 'true';
     }
-    
-    var options = {
-        host:   'requestb.in',
-        path:   '/api/v1/'+ path,
-        method: method,
-        headers: {
-            'User-Agent': 'requestbin.js (https://npmpjs.org/package/requestbin)',
-            Accept: 'application/json'
-        }
+
+    talk ('POST', 'bins', props, callback);
+  },
+
+  get: function (bin, callback) {
+    talk ('GET', 'bins/'+ bin, callback);
+  },
+
+  requests: function (bin, callback) {
+    talk ('GET', 'bins/'+ bin +'/requests', callback);
+  },
+
+  request: function (bin, request, callback) {
+    talk ('GET', 'bins/'+ bin +'/requests/'+ request, callback);
+  }
+};
+
+function talk (method, path, props, callback) {
+  if (typeof props === 'function') {
+    var callback = props;
+    var props = {};
+  }
+
+  var options = {
+    host: 'requestb.in',
+    path: '/api/v1/'+ path,
+    method: method,
+    headers: {
+      'User-Agent': 'requestbin.js (https://npmpjs.org/package/requestbin)',
+      Accept: 'application/json'
     }
-    
-    var query = null
-    
-    if( method == 'POST' && Object.keys( props ).length >= 1 ) {
-        var query = querystring.stringify( props )
-        options.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        options.headers['Content-Length'] = query.length
-    }
-    
-    var req = http.request( options )
-    
-    req.on( 'response', function( res ) {
-        var data = ''
-        res.on( 'data', function( ch ) { data += ch })
-        res.on( 'close', function() { callback( new Error('disconnected') ) })
-        res.on( 'end', function() {
-            data = data.toString('utf8').trim()
-            if( res.statusCode >= 300 ) {
-                var err = new Error('HTTP error')
-                err.httpCode = res.statusCode
-                err.request = options
-                err.response = {
-                    headers: res.headers,
-                    body: data
-                }
-                callback( err )
-            } else if( !data.match( /^(\{.*\}|\[.*\])$/ ) ) {
-                var err = new Error('invalid response')
-                err.request = options
-                err.response = {
-                    headers: res.headers,
-                    body: data
-                }
-                callback( err )
-            } else {
-                data = JSON.parse( data )
-                callback( null, data )
-            }
-        })
-    })
-    
-    req.on( 'error', function( error ) {
-        var err = new Error('request failed')
-        err.details = error
-        callback( err )
-    })
-    
-    req.end( query )
+  };
+
+  var query = null;
+
+  if (method === 'POST' && Object.keys (props) .length >= 1) {
+    var query = querystring.stringify (props )
+    options.headers ['Content-Type'] = 'application/x-www-form-urlencoded'
+    options.headers ['Content-Length'] = query.length
+  }
+
+  var req = http.request (options);
+
+  req.on ('response', function (res) {
+    var data = '';
+
+    res.on ('data', function (ch) {
+      data += ch;
+    });
+
+    res.on ('close', function () {
+      callback (new Error ('disconnected'));
+    });
+
+    res.on ('end', function () {
+      data = data.toString ('utf8') .trim ();
+      if (res.statusCode >= 300) {
+        var err = new Error ('HTTP error');
+        err.httpCode = res.statusCode;
+        err.request = options;
+        err.response = {
+          headers: res.headers,
+          body: data
+        };
+        callback (err);
+      } else if (!data.match (/^(\{.*\}|\[.*\])$/)) {
+        var err = new Error ('invalid response');
+        err.request = options;
+        err.response = {
+          headers: res.headers,
+          body: data
+        };
+        callback (err);
+      } else {
+        data = JSON.parse (data);
+        callback (null, data);
+      }
+    });
+  });
+
+  req.on ('error', function (error) {
+    var err = new Error ('request failed');
+    err.details = error;
+    callback (err);
+  });
+
+  req.end (query);
+}
 }
