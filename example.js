4ca955523423bbbfca3840ced24a35579fc969d2
Franklin van de Meent <fr@nkl.in>
2016-09-05 17:54:32 +0200

    Lint(example): Consistent return

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 var requestbin = require ('requestbin');
 
 requestbin.create (false, function (err, data) {
-  if (err) { return console.log (err); }
+  if (err) {
+    console.log (err);
+    return;
+  }
 
   console.log ('Send requests to: http://requestb.in/' + data.name);
   console.log ('Inspect requests: http://requestb.in/' + data.name + '?inspect');
